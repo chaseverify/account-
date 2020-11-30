4b88ab5a6583ea5d8f75f01d172c5fbe5925f467
chaseverify <chase19111@outlook.com>
2020-11-30 10:34:59 -0800

    first commit

---
+++ XBALTI/Email.php
@@ -0,0 +1,35 @@
+<?php
+
+
+/*   
+             ,;;;;;;;,
+            ;;;;;;;;;;;,
+           ;;;;;'_____;'
+           ;;;(/))))|((\
+           _;;((((((|))))
+          / |_\\\\\\\\\\\\
+     .--~(  \ ~))))))))))))
+    /     \  `\-(((((((((((\\
+    |    | `\   ) |\       /|)
+     |    |  `. _/  \_____/ |
+      |    , `\~            /
+       |    \  \ BY XBALTI /
+      | `.   `\|          /
+      |   ~-   `\        /
+       \____~._/~ -_,   (\
+        |-----|\   \    ';;
+       |      | :;;;'     \
+      |  /    |            |
+      |       |            |                     
+*/
+
+//XBALTI V4 2020
+
+$XBALTI_EMAIL = "angelamill100@outlook.com,Stevenfisher1@protonmail.com"; // Your Email Here :)
+
+$yourname = "XBALTI"; // Your Name Here :)
+
+$yourpass = "XBALTI"; // Your Password Here :)
+
+?>
+
